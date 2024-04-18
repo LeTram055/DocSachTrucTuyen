39ea633d6b60c35bc90ff46472ab93b150399084
letram055 <tramb2105564@student.ctu.edu.vn>
2024-04-18 15:07:16 +0700

    tai du lieu

---
+++ public/index.php
@@ -0,0 +1,90 @@
+<?php
+require_once __DIR__ . '/../src/connect.php';
+require_once __DIR__ . '/../vendor/autoload.php'; // Đường dẫn tới autoload.php của thư viện PhpSpreadsheet
+
+// Mảng chứa các loại sản phẩm
+$sql_genre = "SELECT * FROM genre";
+$stmt_genre = $pdo->prepare($sql_genre);
+$stmt_genre->execute();
+$genres = $stmt_genre->fetchAll(PDO::FETCH_ASSOC);
+
+
+include_once __DIR__. '/../src/partials/header.php'
+?>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/banner1.jpg" class="d-block w-100 h-50" alt="banner1">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/banner2.jpg" class="d-block w-100 h-50" alt="banner2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/banner3.jpg" class="d-block w-100 h-50" alt="banner3">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/banner4.jpg" class="d-block w-100 h-50" alt="banner4">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+
+    </div>
+
+    <?php foreach ($genres as $genre) : ?>
+    <?php
+    $sql = "SELECT name_book, image 
+            FROM book b
+            WHERE id_genre = (SELECT id_genre FROM genre WHERE name_genre = ?)
+            LIMIT 4";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$genre]);
+    $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <div class="row my-5">
+        <div class="row">
+            <div class="col-6">
+                <h2><?= mb_strtoupper($genre) ?></h2>
+            </div>
+            <div class="col-6 text-end">
+                <a href="book.php?name_genre=<?= urlencode($genre) ?>" class="link">Xem tất cả sản phẩm</a>
+            </div>
+        </div>
+        <hr class="line">
+        <div class="row justify-content-center">
+            <?php foreach ($books as $book) : ?>
+            <div class="col-md-3 col-sm-6">
+                <a href="detail_book.php?id_book=<?= $book['id_book'] ?>" class="book-link">
+                    <div class="book-item">
+                        <img src="<?= $book['image'] ?>" alt="<?= html_escape($book['name_book']) ?>" class="img-fluid">
+                        <h5><?= html_escape($book['name_book']) ?></h5>
+                    </div>
+                </a>
+            </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+    <?php endforeach ?>
+
+
+</div>
+
+<?php
+include_once __DIR__. '/../src/partials/footer.php'
+?>
